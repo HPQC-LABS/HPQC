7a8c2cf3fb922f8ee5280aaee8299aef89609dca
Matthew Charbonneau <matthewgcharbonneau@gmail.com>
2021-03-04 18:50:22 -0500

    The following Changes were made:
     Fixed text on current biographies page -> wasn't being displayed
     Implemented biography template for students
      - Formatting still requires work (description and mobile)
      - Template is confined to just Matthew's listing so merge can be made
      without disrupting the websites flow

---
+++ people.js
@@ -0,0 +1,17 @@
+const card = document.querySelectorAll('.itemInner');
+
+card.forEach((card) => {
+     card.addEventListener("click", () => {
+          card.classList.toggle("is-flipped");
+     });
+});
+
+
+var clientHeight = document.getElementById('101').clientHeight;
+var clientWidth = document.getElementById('101').clientWidth;
+console.log(clientHeight);
+console.log(clientWidth);
+
+
+document.getElementById('cardFace').style.height = clientHeight + "px";
+document.getElementById('cardFace').style.width = clientWidth + "px";
