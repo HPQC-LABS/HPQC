941d738455f54f01626ac298672ddc4b4a7c4f4b
Matthew Charbonneau <matthewgcharbonneau@gmail.com>
2021-03-11 19:58:04 -0500

    The Following Changes were made (Don't commit):
    Fixed spacing issue where entries were stacked over one another
    Added a hide feature for those who don't have a bio
    Fixed bio text to fit / adjust to height of text
    
    Cropped some photos to adjust for template
     -(THIS ADDITION HAS BEEN DELETED)

--- people.js
+++ people.js
@@ -2,26 +2,25 @@ const card = document.querySelectorAll('.itemInner');
 
 card.forEach((card) => {
      card.addEventListener("click", () => {
-          card.classList.toggle("is-flipped");
+          /*console.log(card.classList);*/
+          if(card.classList.contains('hideBio') == false){
+               card.classList.toggle("is-flipped");
+          }
      });
 });
 
-/*
-const seeBio = document.getElementsByClassName('showBio');
-if(seeBio.classList.contains('cardBio')){
-     alert("test");
-}
-*/
+
+
+
 
 window.addEventListener('load', function(){
      var clientHeight = document.getElementById('item-main').clientHeight;
      var clientWidth = document.getElementById('item-main').clientWidth;
+     /*
      console.log(clientHeight);
      console.log(clientWidth);
-     
-     
-     /*document.getElementById('cardFace').style.height = clientHeight + "px";*/
      console.log(document.querySelectorAll("#cardFace"));
+     */
      var elements = document.querySelectorAll("#cardFace");
      elements.forEach((elements) => {
           elements.style.width = clientWidth + "px";
@@ -30,14 +29,3 @@ window.addEventListener('load', function(){
      })
 })
 
-/*
-for(var i=0; i< elements.length; i++){
-
-}
-
-/*
-Array.from(elements).forEach(function(elements){
-     elements.style.width = clientWidth + "px";
-     elements.style.height = clientHeight + "px";
-});
-*/
