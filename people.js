25772f10f2d1abdf1fff946367f0a22c5d9cdde2
Matthew Charbonneau <matthewgcharbonneau@gmail.com>
2021-03-12 20:51:50 -0500

    The following Changes were made (don't commit):
    Changed all entries to new format
    Fixed height discrepancy with images that differed in size
    Fixed images appearing in single column issue

--- people.js
+++ people.js
@@ -16,6 +16,8 @@ card.forEach((card) => {
 window.addEventListener('load', function(){
      var clientHeight = document.getElementById('item-main').clientHeight;
      var clientWidth = document.getElementById('item-main').clientWidth;
+     var imgClientHeight = document.getElementById('mainImg').clientHeight;
+     console.log(imgClientHeight);
      /*
      console.log(clientHeight);
      console.log(clientWidth);
@@ -25,6 +27,18 @@ window.addEventListener('load', function(){
      elements.forEach((elements) => {
           elements.style.width = clientWidth + "px";
           elements.style.height = clientHeight + "px";
+     
+     })
+
+     var elements = document.querySelectorAll(".imgSettings");
+     elements.forEach((elements) => {
+          elements.style.height = imgClientHeight + "px";
+     
+     })
+
+     var elements = document.querySelectorAll(".imgSettings2");
+     elements.forEach((elements) => {
+          elements.style.height = imgClientHeight - 10 + "px";
      
      })
 })
