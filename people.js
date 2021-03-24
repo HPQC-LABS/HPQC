41d28f7286332f8cd385fed44e0cf82278e51e01
Matthew Charbonneau <matthewgcharbonneau@gmail.com>
2021-03-23 21:12:09 -0400

    The following Changes were made:
    - Changed links in Dr. Dattani's bio so that they display the name of
    the paper (better accessibility for the viewer of the website)
    - Added a scroll to top button that appears on the side of the screen
    once the used has scrolled past a certain point on the website
        - formatting for mobile has yet to be finished
    - Fixed a formatting issue with the people's grid when at a screen size
    of less than 430px which caused the left hand column to be larger than
    all others

--- people.js
+++ people.js
@@ -1,4 +1,62 @@
 
+$(document).ready(function(){
+     console.log("People page!");
+ 
+     //Initialize the page by displaying the list of entries, and hiding the bios page
+     $('.peopleBio').hide();
+     $('.peopleList').show();
+     $('#backToPeopleList').on("click", backToPeopleList);
+    
+ 
+     
+     /* Code used for previous iteration of the website 
+     let faculty = [
+         {id: 101, firstName: 'Nike',  lastName: 'Dattani',   imgName: 'NikeDattani.png',     title: 'PhD Oxford'},
+         {id: 102, firstName: 'Erin',  lastName: 'Zuo',       imgName: 'ErinZuo.jpg',         title: 'PhD WIT'},
+         {id: 103, firstName: 'Deyan', lastName: 'Mihaylov',  imgName: 'DeyanMihaylov.png',   title: 'PhD Cambridge'}
+     ];
+     */
+ 
+ 
+     //List of ids corresponding to each entries biography 
+     let ids = [101, 102, 103, 104, 105, 106, 107, 108, 109];
+ 
+     //If the div containing the id is click the showBio function will be run 
+     ids.forEach(id => {
+         $(`#${id}`).on("click", showBio);
+     });
+ 
+     //Toggles between displaying the list of people and their biographies
+     function showBio(){
+         console.log("You clicked on id: ", this.id);
+         $('.peopleList').hide();
+         $('.peopleBio').show();
+     };
+ 
+     //Back button again toggles which screen is displayed
+     function backToPeopleList(){
+         $('.peopleList').show();
+         $('.peopleBio').hide();
+     }
+ })
+
+ function scrollToTop(){
+     window.scroll({
+          top: 0, 
+          left: 0, 
+          behavior: 'smooth'
+        });
+}
+
+const button = document.getElementById("buttonID");
+button.addEventListener("click", () => {
+     scrollToTop();
+})
+ 
+ 
+/* 
+Js for Card Flip Template, 
+
 
 /* 
 Js for Card Flip Template
