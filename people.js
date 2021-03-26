239691292effb25ddbdb1c8df525b0f7da2ccb0c
Matthew Charbonneau <matthewgcharbonneau@gmail.com>
2021-03-25 21:06:34 -0400

    The following changes were made:
    -Fixed bug with back button not working when trying to get off of the
    bios page
    - Commented css, html, & js for back button and scroll to top
    - Added scroll to top feature to the LaTeX page for better accessibility
    - Centered dropdown menu for Resources on mobile

--- people.js
+++ people.js
@@ -26,33 +26,81 @@ $(document).ready(function(){
          $(`#${id}`).on("click", showBio);
      });
  
+     //Sleep function allows implementation of a delay in the running of code
+     function sleep (time){
+          return new Promise((resolve) => setTimeout(resolve,time));
+      }
+
+
+     //Variable used to indicate whether bio is toggled or not
+     var bioToggle = 0;
      //Toggles between displaying the list of people and their biographies
      function showBio(){
-         console.log("You clicked on id: ", this.id);
-         $('.peopleList').hide();
-         $('.peopleBio').show();
+          console.log("You clicked on id: ", this.id);
+          
+          $('.peopleList').hide();
+          $('.peopleBio').show();
+          //Allowing page to load before running
+          sleep(100).then(() => {
+               setToggle(1);
+           });
+          
      };
  
      //Back button again toggles which screen is displayed
      function backToPeopleList(){
-         $('.peopleList').show();
-         $('.peopleBio').hide();
+          $('.peopleList').show();
+          $('.peopleBio').hide();
+          sleep(100).then(() => {
+               setToggle(0);
+           });  
      }
  })
 
- function scrollToTop(){
+//Function scrolls page to the indicated position
+function scrollToTop(){
      window.scroll({
-          top: 500, 
+          top: 500, //Set to 500 so that it stops with the back button on screen
           left: 0, 
           behavior: 'smooth'
         });
 }
 
+//Runs scrollToTop function on click of the scroll button
 const button = document.getElementById("buttonID");
 button.addEventListener("click", () => {
      scrollToTop();
 })
  
+//Set function sets the value of variable bioToggle for use within other functions
+function setToggle(x){
+     bioToggle = x
+}
+
+//get function returns the value of variable bioToggle for use within other functions
+function getToggle(){
+     return bioToggle;
+}
+
+//The purpose of the following section is to fix the error in which 
+//pressing the back button after going to the bio section would not
+// actually send the user back to the list but instead stay on the bios
+//page and go to the top it (effectively the same as refreshing a normal page)
+
+
+//function to run when the back button is pressed while on the people page
+$(window).on('popstate', function(event) {
+     //Test to see if value returned is correct
+     console.log(getToggle()); 
+     //Conditional statement that only applies the following code if on the bios page
+     if(getToggle() == 1){
+          //Toggles the visibility of bios so that the peopleList is visible
+          $('.peopleList').show();
+          $('.peopleBio').hide();
+          //Reset value of Toggle as the page has been changed
+          setToggle(0);
+     }
+});
  
 /* 
 Js for Card Flip Template, 
